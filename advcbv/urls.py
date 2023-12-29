13d8ad1e00011693dd4fd23b1824b0be4e082f4f
Kevin Harris <kevin.harris@diaceutics.com>
2023-12-29 12:47:34 -0500

    Updates to work with django 4.1

--- advcbv/urls.py
+++ advcbv/urls.py
@@ -13,14 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import include
 from django.contrib import admin
+from django.urls import path
+
 from basic_app import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls,name='admin'),
-    url(r'^$',views.IndexView.as_view()),
-    url(r'^basic_app/',include('basic_app.urls',namespace='basic_app')),
+    path(r'^admin/', admin.site.urls, name='admin'),
+    path(r'^$', views.IndexView.as_view()),
+    path(r'^basic_app/', include('basic_app.urls', namespace='basic_app')),
     # url(r'^$',views.CBView.as_view()),
     # url(r'^$',views.index)
 ]
