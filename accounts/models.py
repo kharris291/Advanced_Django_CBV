3d85f1513e6847a0dd2ed0d989589528a0e57b24
Kevin Harris <kevin.harris@diaceutics.com>
2024-01-01 14:33:08 -0500

    lecture 177
    - adding forms
    - adding models
    - adding templates

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User as AuthUser, PermissionsMixin
 from django.db import models
+from django.contrib import auth
+
 
 # Create your models here.
+class User(AuthUser, PermissionsMixin):
+    def __str__(self):
+        return "@{}".format(self.username)
